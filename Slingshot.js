8e880db0e538e24d3024e500195aba5008804530
unknown <stevebutti07@gmail.com>
2020-09-24 19:33:16 -0400

    no message

--- Slingshot.js
+++ Slingshot.js
@@ -8,7 +8,11 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
         World.add(world, this.sling);
+        
     }
 
     fly(){
@@ -16,11 +20,16 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1, 200,20);
+        image(this.sling2, 173,20);
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48,39,14);
+            strokeWeight(10);
+            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y);
+            line(pointA.x-20,pointA.y, pointB.x-10, pointB.y);
+            image(this.sling3,pointA.x-30,pointA.y-10,15,30);
         }
     }
     
